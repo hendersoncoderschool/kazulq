8a1f111dd14b56994a2da7de247e8c87d6a110b9
Henderson The Coder School <68570781+hendersoncoderschool@users.noreply.github.com>
2023-07-21 09:45:22 -0700

    test

--- app/page.js
+++ app/page.js
@@ -1,95 +1,41 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import './globals.css'
 
-export default function Home() {
+export default function App() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <div id = 'header'>
+      </div>
+      <h1>♦Kazulq♦</h1>
+
+      <div class="float-container">
+        
+        <div class = "float-child">
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <p>Kazulq has a variety of movies to watch and new movies come out every month. So many movies for a low price of nothing. 
+            There may be a few pop up adds but just make 
+            sure to not click on anything
+            for it is probally a virus.</p>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+        
+        <div class = "float-child">
+          
+          <p>Most Popular (Adventure): 
+          <br></br>
+            The Terminator
           </p>
-        </a>
+          <img src= "Terminator.jpg" width = "100%"></img >
+        </div>
+        
       </div>
+      <nav>
+        <a href = "#"><i class='fas fa-home'></i></a>
+        <a href = "#"><i class='fas fa-search'></i></a>
+        <a href = "#"><i class='far fa-bookmark'></i></a>
+        <a href = "#"><i class='fab fa-creative-commons-by'></i></a>
+      </nav>
     </main>
   )
 }
